d4d9d9bab2c915d02182876d7f880e80a947ab02
Joy Foster (IMac) <joy@intelesense.net>
2019-05-23 13:40:43 -0500

    Initial Project

---
+++ src/main/java/com/example/joy/servletLogin/App.java
@@ -0,0 +1,13 @@
+package com.example.joy.servletLogin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
